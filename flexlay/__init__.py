89603b323727e133b85de8a5a0cf9fe2ae75efc3
AnatolyStev <280393124+AnatolyStev@users.noreply.github.com>
2026-09-11 00:51:37 +0100

    two (it didnt work)

--- flexlay/__init__.py
+++ flexlay/__init__.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# test change
 
 from .util.config import Config
 # from .stroke import Dab, Stroke
